c53d882be285f45028a9c18e031fbcfe3eaf02ed
Hadoop user <hduser@rajan-vpc.(none)>
2015-05-14 05:42:38 +0545

    scripts added

---
+++ stripes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hadoop fs -mkdir rfinput
+hadoop fs -mkdir rfoutput
+
+hadoop fs -put src/main/resources/* rfinput
+
+hadoop jar target/WordCountHadoopProject-0.0.1-SNAPSHOT.jar com.hadoop.relativefrequency.stripes.RelativeFrequencyTool -D mapred.reduce.tasks=1 rfinput/*.txt rfoutput
+
+hadoop fs -get rfoutput/* output/stripes
